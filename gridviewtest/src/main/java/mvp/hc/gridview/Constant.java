a14281ebad5f2f34904ae83af000498918a57687
15609075281 <676122503@qq.com>
2018-03-22 11:52:38 +0800

    增加人脸识别
    增加人脸识别

---
+++ gridviewtest/src/main/java/mvp/hc/gridview/Constant.java
@@ -0,0 +1,10 @@
+package mvp.hc.gridview;
+
+/**
+ * Created by zhangyi on 16/3/26.
+ */
+public class Constant {
+     //设置两个之前获取的两个常量
+     public static final String Key="k4bFXjE5P9pNeBux0wi7KkIJQxXi18Ik";//你的key
+     public static final String Secret="ruziLd9QSZ7aYsSmHe1E6MF5MBD9HroR";//你的Secret
+}
